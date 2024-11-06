773d51241a5ca522f0db83c6cf3e8d03839759d6
jasonramirez7 <jasonramirez7777@outlook.com>
2024-11-06 10:02:28 -0600

    update

--- src/lib/layouts/home/Adsense.tsx
+++ src/lib/layouts/home/Adsense.tsx
@@ -1,42 +1,12 @@
 'use client'
 
 import { useEffect } from "react";
-import FreestarAdSlot from '@freestar/pubfig-adslot-react-component'
-
 
 export default function Adsense({ dataAdSlot, dataAdClient }: { dataAdSlot: string, dataAdClient: string }) {
-
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined' && window.adsbygoogle) {
-  //     window.adsbygoogle.push({});
-  //   }
-  // }, []);
-
-  const placementName = 'PublisherName_970x250_728x90_320x50'
-    const slotId = 'in_content_ad_1'
-    const publisher = 'publisherName'
-    const targeting = { key1: 'value1', key2: 'value2' }
-    
-
 
   return <div>
     <div data-freestar-ad="__336x280 __970x250 __640x360"
       id="fifarosters_leaderboard_atf"
       data-google-query-id="CMnJ-87yx4kDFS48RAgdFVsIgQ" />
-
-    <div>
-      <FreestarAdSlot
-        publisher={publisher}
-        placementName={placementName}
-        slotId={slotId}
-        targeting={targeting}
-        channel='custom_channel'
-        classList={['m-30', 'p-15', 'b-thin-red']}
-        onNewAdSlotsHook={(placementName) => console.log('creating ad', placementName)}
-        onDeleteAdSlotsHook={(placementName) => console.log('destroying ad', placementName)}
-        onAdRefreshHook={(placementName) => console.log('refreshing ad', placementName)}
-      />
-     
-    </div>
   </div>
 }
--- src/lib/layouts/home/HomeAd.tsx
+++ src/lib/layouts/home/HomeAd.tsx
@@ -9,6 +9,7 @@ export default function HomeAd() {
   const homeAdSlot = "8083460066"
 
   return <div className="bg-gray-100 min-h-60 flex justify-center items-center">
+
     <h1>Home Ad here</h1>
     <Adsense dataAdClient={adClient} dataAdSlot={homeAdSlot} />
   </div>
